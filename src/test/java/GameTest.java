ab467ddbf432d5172c7c393223ef363ba02790b9
rikikun <pondpond_arsenal@hotmail.com>
2014-05-02 15:25:26 +0700

    finish basic Test GameUnit

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,9 +1,13 @@
-
 public class GameTest {
 
 	public String powYingChub(String player1, String player2) {
-		if(player1==player2){
+		if (player1 == player2) {
 			return "Draw";
+		}
+		if ((player1 == "H" && player2 == "P")
+				|| (player1 == "P" && player2 == "S")
+				|| (player1 == "S" && player2 == "H")) {
+			return "Player2";
 		}
 		return "Player1";
 	}
--- src/test/java/TestHammerSP.java
+++ src/test/java/TestHammerSP.java
@@ -7,17 +7,30 @@ public class TestHammerSP {
 
 	
 	@Test
-	public void CheckResultHammerScissorsReturnPlayer1Win(){
+	public void checkResultHammerScissorsReturnPlayer1Win(){
 		assertEquals("Check result", "Player1",new GameTest().powYingChub("H","S"));
 	}
 	
 	@Test
-	public void CheckResultHammerHammerReturnPlayerDraw(){
+	public void checkResultHammerHammerReturnPlayerDraw(){
 		assertEquals("Check result", "Draw",new GameTest().powYingChub("H","H"));
 	}
 	
 	@Test
-	public void CheckResultHammerPaperReturnPlayer2Win(){
+	public void checkResultHammerPaperReturnPlayer2Win(){
 		assertEquals("Check result", "Player2",new GameTest().powYingChub("H","P"));
 	}
+	
+	@Test
+	public void checkResultPaperScissorsReturnPlayer2Win(){
+		assertEquals("Check result", "Player2",new GameTest().powYingChub("P","S"));
+	}
+	
+	@Test
+	public void checkResultScissorsHammerReturnPlayer2Win(){
+		assertEquals("Check result", "Player2",new GameTest().powYingChub("S","H"));
+	}
+	
+	
+	
 }
